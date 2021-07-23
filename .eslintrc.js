312d99e974907697b5f2d4c739dbae65546d9402
Ivan Garcia <iegc71@gmail.com>
2021-07-23 12:35:22 -0500

    folder structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,46 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint", "prettier"],
-  rules: {
-    "prettier/prettier": "error",
-    "no-use-before-define": "off",
-    "react/jsx-filename-extension": [
-      1,
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-    ],
-  },
-  settings: {
-    "import/resolver": {
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
-    },
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+		'plugin:prettier/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint', 'prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+		'arrow-body-style': 'off',
+		'prefer-arrow-callback': 'off',
+		'no-use-before-define': 'off',
+		'react/jsx-filename-extension': [
+			1,
+			{ extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+		],
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				ts: 'never',
+				tsx: 'never',
+			},
+		],
+		'func-names': ['error', 'never'],
+	},
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			},
+		},
+	},
 };
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		env: {
+			production: {
+				plugins: ['react-native-paper/babel'],
+			},
+		},
+		// CONFIGURAR LOS ALIAS
+		plugins: [
+			[
+				'module-resolver',
+				{
+					root: ['./'],
+					alias: {
+						assets: './assets',
+						components: './src/components',
+						elements: './src/elements',
+						screens: './src/screens',
+						navigation: './src/navigation',
+						utils: './src/utils',
+					},
+				},
+			],
+		],
+	};
 };
